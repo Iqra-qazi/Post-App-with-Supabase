2885fb7cec818e4e606c2f182cf9da329c22a3c3
Iqra-qazi <iqraq577@gmail.com>
2026-07-03 03:14:39 +0500

    supabase

---
+++ Posteura-POSTAPP/app.js
@@ -0,0 +1,281 @@
+var supabase = window.supabase.createClient('https://krfehxcloabvtbdxlzwy.supabase.co', 'sb_publishable_ViZdvxOYXLv__IAiizr_ng_k7zffPSM')
+console.log(supabase);
+let edited = false;
+let idindex = null;
+
+window.onload = async function () {
+
+    const posts = document.getElementById("posts");
+
+    if (!posts) {
+        console.log("posts div not found");
+        return;
+    }
+
+    try {
+
+        const { data, error } = await supabase
+            .from("PostApp")
+            .select("*")
+            .order("id", { ascending: false });
+
+        if (error) {
+            console.log(error);
+            return;
+        }
+
+        posts.innerHTML = "";
+
+        data.forEach(post => {
+
+            posts.innerHTML += `
+            <div class="card m-3">
+                <div class="card-header">Post: @${post.email}</div>
+
+                <div class="card-body"
+                style="background-image:url('${post.cardBg}');
+                background-size:cover;">
+
+                    <h5>${post.title}</h5>
+
+                    <p>${post.description}</p>
+
+                </div>
+
+                <div class="m-3 ms-auto">
+
+                <button class="btn btn-success"
+                onclick="editedPost(event,${post.id})">
+                Edit
+                </button>
+
+                <button class="btn btn-danger"
+                onclick="deletePost(event,${post.id})">
+                Delete
+                </button>
+
+                </div>
+
+            </div>
+            `;
+
+        });
+
+    } catch (err) {
+
+        console.log(err);
+
+    }
+
+}
+
+async function searchbtn() {
+    let searchInput= document.getElementById("searchInput").value;
+    console.log(searchInput);
+      try {
+
+  const { data, error } = await supabase
+  .from('PostApp')
+  .select('*')
+  .or(`title.ilike.%${searchInput}%,description.ilike.%${searchInput}%`)
+
+  var posts = document.getElementById("posts")
+  console.log(data);
+console.log(error);
+  posts.innerHTML = ""
+  data.forEach(post => {
+    posts.innerHTML += `
+    <div class="card mb-2">
+             <div class="card-header">${post.id} ~Post</div>
+             <div style="background-image:url(${post.cardBg})" class="card-body">
+               <figure>
+                 <blockquote class="blockquote">
+                   <p>
+                     ${post.title}
+                   </p>
+                 </blockquote>
+                 <figcaption class="blockquote-footer">
+                   ${post.description}
+                 </figcaption>
+               </figure>
+             </div>
+             <div class="ms-auto m-2">
+             <button onclick="editPost(event,${post.id},'${post.description}','${post.title}','${post.cardBg}')" class="btn btn-success">Edit</button>
+             <button onclick="deletePost(event,${post.id})" class="btn btn-danger">Delete</button>
+             </div>
+           </div>
+   `})
+
+  console.log(data);
+//   if(!data.length){
+//     posts.innerHTML= "No posts Found"
+if (data.length === 0) {
+    posts.innerHTML = `
+        <div class="not-found-card">
+            <div class="search-icon">🔍</div>
+            <h3>No Posts Found</h3>
+            <p>No posts match "<b>${searchInput}</b>"</p>
+            <button onclick="window.location.reload()" class="btn btn-primary">
+                Show All Posts
+            </button>
+        </div>
+    `;
+    return;
+
+
+  }
+
+  if(error) console.log(error);
+  } catch (error) {
+    console.log(error);
+  }
+
+}
+var cardBg = "";
+
+function select(src, element) {
+    cardBg = src;
+
+    var images = document.getElementsByClassName("cardimg");
+
+    for (var i = 0; i < images.length; i++) {
+        images[i].classList.remove("select");
+    }
+
+    element.classList.add("select");
+}
+
+
+async function post() {
+
+    const title = document.getElementById("title").value.trim();
+    const description = document.getElementById("description").value.trim();
+
+    if (!title || !description || !cardBg) {
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Please fill all fields and select an image!"
+        });
+        return;
+    }
+      try {
+
+      const { data: { user }, error } = await supabase.auth.getUser()
+      console.log(user.email);
+      email = user.email
+      if(error)console.log(error);
+
+    } catch (error) {
+      console.log(error);
+    }
+
+    try {
+
+        if (edited) {
+            const { error } = await supabase
+                .from("PostApp")
+                .update({
+                    title: title,
+                    description: description,
+                    bg_img: cardBg
+                })
+                .eq("id", idindex);
+
+            if (error) {
+                console.log(error);
+                return;
+            }
+
+            edited = false;
+            idindex = null;
+
+            Swal.fire({
+                icon: "success",
+                title: "Post Updated Successfully"
+            });
+
+        } else {
+
+            const { error } = await supabase
+                .from("PostApp")
+                .insert({
+                    title: title,
+                    description: description,
+                    bg_img: cardBg
+                });
+
+            if (error) {
+                console.log(error);
+                return;
+            }
+
+            Swal.fire({
+                icon: "success",
+                title: "Post Added Successfully"
+            });
+
+        }
+
+        document.getElementById("title").value = "";
+        document.getElementById("description").value = "";
+        cardBg = "";
+
+        window.location.reload();
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+async function deletePost(event, id) {
+    try {
+        const { data, error } = await supabase
+            .from('PostApp')
+            .delete()
+            .eq('id', id)
+        if (error) console.log(error);
+
+    } catch (error) {
+        console.log(error);
+    }
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "This post will be deleted!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#d33',
+        cancelButtonColor: '#3085d6',
+        confirmButtonText: 'Yes, delete it!',
+        cancelButtonText: 'Cancel'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            // Delete the card
+
+            var card = event.target.closest('.card'); // parent card div
+            card.remove();
+
+            // Success alert
+            Swal.fire(
+                'Deleted!',
+                'Your post has been deleted.',
+                'success'
+            );
+        }
+    });
+}
+
+function editedPost(event, id) {
+
+    var card = event.target.closest(".card");
+
+    var title = card.querySelector("h5").innerText;
+    var description = card.querySelector("p").innerText;
+
+    document.getElementById("title").value = title;
+    document.getElementById("description").value = description;
+
+    edited = true;
+    idindex = id;
+}
---
+++ Posteura-POSTAPP/auth.js
@@ -0,0 +1,106 @@
+var supabase = window.supabase.createClient('https://krfehxcloabvtbdxlzwy.supabase.co', 'sb_publishable_ViZdvxOYXLv__IAiizr_ng_k7zffPSM')
+console.log(supabase);
+function clickHere(){
+    window.location.href="login.html"
+}
+async function login() {
+
+    const name = document.getElementById("fname").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    if (!name || !email || !password) {
+        Swal.fire("Error", "Fill all fields", "error");
+        return;
+    }
+
+    const { data, error } = await supabase.auth.signUp({
+
+        email: email,
+
+        password: password,
+
+        options: {
+
+            data: {
+
+                username: name
+
+            }
+
+        }
+
+    });
+
+    if (error) {
+
+        Swal.fire("Error", error.message, "error");
+
+    } else {
+
+        Swal.fire("Success", "Account Created Successfully", "success")
+            .then(() => {
+
+                location.href = "login.html";
+
+            });
+
+    }
+
+}
+async function loginanother() {
+
+    const email = document.getElementById("email2").value;
+
+    const password = document.getElementById("password2").value;
+
+    if (!email || !password) {
+
+        Swal.fire("Error", "Fill all fields", "error");
+
+        return;
+
+    }
+
+    const { data, error } = await supabase.auth.signInWithPassword({
+
+        email: email,
+
+        password: password
+
+    });
+
+    if (error) {
+
+        Swal.fire("Error", error.message, "error");
+
+    } else {
+
+        Swal.fire("Success", "Login Successful", "success")
+            .then(() => {
+
+                location.href = "home.html";
+
+            });
+
+    }
+
+}
+function signanother() {
+
+    location.href = "signup.html";
+
+}
+// 
+const { data } = supabase.auth.onAuthStateChange((event, session) => {
+  console.log(event, session)
+
+//   if (event === 'INITIAL_SESSION') {
+//    if(!session)alert("Please create account firstly")
+//   } else if (event === 'SIGNED_IN') {
+//     alert("Successfully SignUp")
+//     location="home.html"
+//   } 
+  
+})
+
